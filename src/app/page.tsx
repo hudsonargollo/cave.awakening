88a0c5f5d049e2618ea6449de52b85e24908a725
Hudson Argollo <105505660+hudsonargollo@users.noreply.github.com>
2025-04-29 02:01:54 -0300

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import OnboardingContainer from '@/components/onboarding/onboarding-container'
-
-export default function Home() {
-  return (
-    <main>
-      <OnboardingContainer />
-    </main>
-  )
-}
