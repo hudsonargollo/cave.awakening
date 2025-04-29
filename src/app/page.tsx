4dd903e5dab35fe5d7d0aa10801fa4cfdd3070d5
Hudson Argollo <105505660+hudsonargollo@users.noreply.github.com>
2025-04-29 02:37:10 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import OnboardingContainer from '@/components/onboarding/onboardingcontainer'
+import OnboardingContainer from '@/components/onboarding/OnboardingContainer'
 
 export default function Home() {
   return (
